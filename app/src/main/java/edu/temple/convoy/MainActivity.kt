ba48652e313f1e1c833a546c46c5096ba8b10260
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-02-09 21:18:37 -0500

    Created composable functions for the screen users will see first after installation, the "Create an Account" screen, and the "Log In" screen

--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -3,14 +3,24 @@ package edu.temple.convoy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import edu.temple.convoy.ui.theme.ConvoyTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +28,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConvoyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    ConvoyApp()
                 }
             }
         }
@@ -30,17 +37,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ConvoyApp() {
+    var loggedIn by remember {
+        mutableStateOf(false)
+    }
+
+    if (!loggedIn) {
+        FirstTimeScreen()
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    ConvoyTheme {
-        Greeting("Android")
+fun FirstTimeScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Create an Account")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Log In")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreateAccountScreen() {
+    var fullName by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = fullName,
+            onValueChange = { fullName = it },
+            label = { Text(text = "Full Name") }
+        )
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text(text = "Username") }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") }
+        )
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text(text = "Confirm Password") }
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Create Account")
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun LogInScreen() {
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text(text = "Username") }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") }
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Log In")
+        }
     }
 }
