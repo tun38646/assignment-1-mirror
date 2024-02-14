4e539aab42e79c9c457c5c20a012241f086b1d8b
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-02-14 14:05:59 -0500

    Updated the navigation flow

--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -12,22 +12,30 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import edu.temple.convoy.ui.theme.ConvoyTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ConvoyTheme {
                 // A surface container using the 'background' color from the theme
@@ -75,24 +83,45 @@ fun InitialScreen(navController: NavController) {
 
 @Composable
 fun CreateAccountScreen(navController: NavController) {
-    var fullName by remember { mutableStateOf("") }
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
+    var username by remember {
+        mutableStateOf("")
+    }
+    var firstName by remember {
+        mutableStateOf("")
+    }
+    var lastName by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var confirmPassword by remember {
+        mutableStateOf("")
+    }
+
+    var errorMessage by remember {
+        mutableStateOf("")
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(
-            value = fullName,
-            onValueChange = { fullName = it },
-            label = { Text(text = "Full Name") }
-        )
+        Text(text = errorMessage, color = Color.Red)
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
             label = { Text(text = "Username") }
+        )
+        OutlinedTextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text(text = "First Name") }
+        )
+        OutlinedTextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text(text = "Last Name") }
         )
         OutlinedTextField(
             value = password,
@@ -104,7 +133,16 @@ fun CreateAccountScreen(navController: NavController) {
             onValueChange = { confirmPassword = it },
             label = { Text(text = "Confirm Password") }
         )
-        Button(onClick = { navController.navigate("mainScreen") }) {
+        Button(onClick = {
+            if (username.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                errorMessage = "Please fill in all fields."
+            } else if (password != confirmPassword) {
+                errorMessage = "Passwords do not match."
+            } else {
+//                navController.navigate("mainScreen")
+            }
+
+        }) {
             Text(text = "Create Account")
         }
     }
@@ -120,10 +158,15 @@ fun LoginScreen(navController: NavController) {
         mutableStateOf("")
     }
 
+    var errorMessage by remember {
+        mutableStateOf("")
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Text(text = errorMessage, color = Color.Red)
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
@@ -134,7 +177,13 @@ fun LoginScreen(navController: NavController) {
             onValueChange = { password = it },
             label = { Text(text = "Password") }
         )
-        Button(onClick = { navController.navigate("mainScreen") }) {
+        Button(onClick = {
+            if (username.isEmpty() || password.isEmpty()) {
+                errorMessage = "Please fill in all fields."
+            } else {
+//                navController.navigate("mainScreen")
+            }
+        }) {
             Text(text = "Log In")
         }
     }
@@ -142,13 +191,18 @@ fun LoginScreen(navController: NavController) {
 
 @Composable
 fun MainScreen(navController: NavController) {
-
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    ConvoyTheme {
-        ConvoyApp()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Main Screen")
     }
 }
+
+//@Preview
+//@Composable
+//fun DefaultPreview() {
+//    ConvoyTheme {
+//        ConvoyApp()
+//    }
+//}
